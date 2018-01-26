493e54fe61c891fa8dbe72f726036f7bf74c3ae5
Björn Nilsved <nilsved@gmail.com>
2018-01-26 16:32:27 +0100

    Remove js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/')
-   .sass('src/app.scss', 'dist/');
+mix.sass('src/app.scss', 'dist/');
 
 // Full API
 // mix.js(src, output);
