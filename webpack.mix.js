498f382b48c4eaa1fa6a1ba2ca59c30dc489bebf
Björn Nilsved <nilsved@gmail.com>
2018-01-29 15:14:44 +0100

    Generate all level css

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,9 @@ let mix = require('laravel-mix');
 mix.setPublicPath('public/');
 
 mix.sass('sass/_L1-reset.scss', 'kingdom-L1.css');
+mix.sass('sass/_L2-spacing.scss', 'kingdom-L2.css');
+mix.sass('sass/_L3-rhythm.scss', 'kingdom-L3.css');
+mix.sass('sass/_L4-baseline.scss', 'kingdom-L4.css');
 
 for (i = 1; i <= 4; i++) {
   mix.combine(
