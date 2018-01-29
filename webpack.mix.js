b6dac65449c4884a44445ed8f451223ba5a54075
Björn Nilsved <nilsved@gmail.com>
2018-01-29 13:33:22 +0100

    Build Level 1 demo pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,33 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.sass('src/app.scss', 'dist/');
+mix.setPublicPath('public/');
+
+mix.sass('sass/_L1-reset.scss', 'L1-kingdom.css');
+
+mix.combine(
+  [
+    'resources/html/html-begin.html',
+    'resources/html/head-begin.html',
+    'resources/html/demo/L1-head.html',
+    'resources/html/head-end.html',
+    'resources/html/demo/body-example.html',
+    'resources/html/html-end.html'
+  ],
+  'public/L1-demo.html'
+);
+
+mix.combine(
+  [
+    'resources/html/html-begin.html',
+    'resources/html/head-begin.html',
+    'resources/html/demo/head.html',
+    'resources/html/head-end.html',
+    'resources/html/demo/body.html',
+    'resources/html/html-end.html'
+  ],
+  'public/demo.html'
+);
 
 // Full API
 // mix.js(src, output);
