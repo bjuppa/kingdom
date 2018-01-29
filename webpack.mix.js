dcc66bd3bf7c6fbd0ed375dd8e7d108c7bfde6a8
Björn Nilsved <nilsved@gmail.com>
2018-01-29 14:22:56 +0100

    Rename level-files

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,19 +2,21 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('public/');
 
-mix.sass('sass/_L1-reset.scss', 'L1-kingdom.css');
+mix.sass('sass/_L1-reset.scss', 'kingdom-L1.css');
 
-mix.combine(
-  [
-    'resources/html/html-begin.html',
-    'resources/html/head-begin.html',
-    'resources/html/demo/L1-head.html',
-    'resources/html/head-end.html',
-    'resources/html/demo/body-example.html',
-    'resources/html/html-end.html'
-  ],
-  'public/L1-demo.html'
-);
+for (i = 1; i <= 4; i++) {
+  mix.combine(
+    [
+      'resources/html/html-begin.html',
+      'resources/html/head-begin.html',
+      'resources/html/demo/L' + i + '-head.html',
+      'resources/html/head-end.html',
+      'resources/html/demo/body-example.html',
+      'resources/html/html-end.html'
+    ],
+    'public/demo-L' + i + '.html'
+  );
+}
 
 mix.combine(
   [
