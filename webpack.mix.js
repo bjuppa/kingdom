412200b8c71cc629bfe9947d25d284c128a3783a
Björn Nilsved <nilsved@gmail.com>
2018-02-04 18:11:29 +0100

    Include utilities in demo

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,12 +2,12 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('public/');
 
-mix.sass('sass/_L1-reset.scss', 'kingdom-L1.css');
-mix.sass('sass/_L2-spacing.scss', 'kingdom-L2.css');
-mix.sass('sass/_L3-rhythm.scss', 'kingdom-L3.css');
-mix.sass('sass/_L4-baseline.scss', 'kingdom-L4.css');
+mix.sass('resources/sass/L1.scss', 'kingdom-L1.css');
+mix.sass('resources/sass/L2.scss', 'kingdom-L2.css');
+mix.sass('resources/sass/L3.scss', 'kingdom-L3.css');
+mix.sass('resources/sass/L4.scss', 'kingdom-L4.css');
 
-for (i = 1; i <= 4; i++) {
+for (let i = 1; i <= 4; i++) {
   mix.combine(
     [
       'resources/html/html-begin.html',
