835edaed7f0a9b136c9891b354bf8f14c2a0e0d8
Björn Nilsved <nilsved@gmail.com>
2018-03-19 11:07:36 +0100

    Setup "Hello world" with Nuxt, generating to /docs for github pages

---
+++ nuxt.config.js
@@ -0,0 +1,22 @@
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/kingdom/'
+  }
+} : {};
+
+module.exports = {
+  srcDir: 'nuxt',
+  generate: {
+    dir: 'docs',
+  },
+  ...routerBase,
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    title: 'Kingdom CSS'
+  }
+};
