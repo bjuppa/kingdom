403e1c59c88c346eb4604c6ccbd7ca58d08f563e
Björn Nilsved <nilsved@gmail.com>
2018-03-20 15:33:42 +0100

    Remove Laravel Mix

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('nuxt/static');
-
-for (let i = 1; i <= 4; i++) {
-  mix.sass('nuxt/assets/L' + i + '.scss', 'kingdom-L' + i + '.css');
-  mix.sass('nuxt/assets/forms-L' + i + '.scss', 'kingdom-forms-L' + i + '.css');
-}
