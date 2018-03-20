2af9b54bce1bb22d970fad15a9d6996ca73aa296
Björn Nilsved <nilsved@gmail.com>
2018-03-20 10:22:47 +0100

    Expand the test index page example

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Hello world!</h1>
+    <p>Intro...</p>
+  </div>
 </template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: 'Test title',
+        link: [
+          { rel: 'stylesheet', href: 'test.css' }
+        ]
+      }
+    }
+  }
+</script>
