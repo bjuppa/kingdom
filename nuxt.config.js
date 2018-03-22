a31873d66c40c5dbe649a710e9fea091025bd395
Björn Nilsved <nilsved@gmail.com>
2018-03-22 13:58:04 +0100

    Turn off scss minification

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,7 @@ module.exports = {
   srcDir: 'nuxt',
   generate: {
     dir: 'docs',
+    minify: false,
   },
   ...routerConfig,
   head: {
@@ -20,6 +21,7 @@ module.exports = {
     title: 'Kingdom CSS'
   },
   build: {
+    cssSourceMap: true,
     postcss: {
       plugins: {
         // Customize `postcss-cssnext` default options
@@ -27,11 +29,19 @@ module.exports = {
           features: {
             // The custom properties plugin won't have preserve set to default until 7.0.0
             // https://github.com/postcss/postcss-custom-properties
-            // TODO: remove this override when postcss-custom-properties 7.0.0 is included
-            customProperties: { preserve: true },
+            // Even with preserve set to true, custom properties plugin adds a lot of duplication
+            // That noise that is removed by minification, but as we have minification turned off on this site we'll also keep custom properties postcss off
+            customProperties: false,
           }
         }
       }
+    },
+    extend (config) {
+      const vueLoader = config.module.rules.find((rule) => rule.loader === 'vue-loader');
+      //console.log(vueLoader.options.loaders);
+      const cssLoader = vueLoader.options.loaders.scss.find((loader) => loader.loader === 'css-loader');
+      //console.log(cssLoader);
+      cssLoader.options.minimize = false;
     }
   }
 };
