2f51680323276bb8cbd32af3c6d47f8baa2363de
Björn Nilsved <nilsved@gmail.com>
2018-03-29 10:48:37 +0200

    Add demo pages with Nuxt

---
+++ nuxt/pages/demo/L4.vue
@@ -0,0 +1,17 @@
+<template>
+  <demo></demo>
+</template>
+
+<script>
+  import Demo from '~/components/demo.vue';
+
+  export default {
+    components: {
+      Demo
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "~assets/L4.scss";
+</style>
---
+++ nuxt/pages/demo/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="demo">
+    <iframe src="demo/L1"></iframe>
+    <iframe src="demo/L2"></iframe>
+    <iframe src="demo/L3"></iframe>
+    <iframe src="demo/L4"></iframe>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "~assets/demo-wrapper";
+</style>
