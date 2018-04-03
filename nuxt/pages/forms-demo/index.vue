11a08fab34192fd0fcf40c405dba8fa8d38feb20
Björn Nilsved <nilsved@gmail.com>
2018-04-03 09:41:02 +0200

    Use absolute path to demo iframes

--- nuxt/pages/forms-demo/index.vue
+++ nuxt/pages/forms-demo/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="demo">
-    <iframe src="forms-demo/L1"></iframe>
-    <iframe src="forms-demo/L2"></iframe>
-    <iframe src="forms-demo/L3"></iframe>
-    <iframe src="forms-demo/L4"></iframe>
+    <iframe src="/forms-demo/L1"></iframe>
+    <iframe src="/forms-demo/L2"></iframe>
+    <iframe src="/forms-demo/L3"></iframe>
+    <iframe src="/forms-demo/L4"></iframe>
   </div>
 </template>
 
