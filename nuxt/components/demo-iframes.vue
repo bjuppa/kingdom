3ba16eec9b034c1072adda62e4347af0e1970a05
Björn Nilsved <nilsved@gmail.com>
2018-04-04 11:16:58 +0200

    Move css into component

--- nuxt/components/demo-iframes.vue
+++ nuxt/components/demo-iframes.vue
@@ -7,7 +7,7 @@
 <script>
   export default {
     computed: {
-      iframes() {
+      iframes () {
         let hrefs = [];
         for (let i = 1; i < 5; i++) {
           hrefs.push(this.$router.resolve(`L${i}/`, null, true).href);
@@ -19,5 +19,21 @@
 </script>
 
 <style lang="scss">
-    @import "~assets/demo-wrapper";
+    @import "~assets/L1";
+</style>
+
+<style lang="scss" scoped>
+    /* Position the demo iframes */
+    .demo {
+        display: flex;
+        height: 100%;
+        height: 100vh;
+
+        > * {
+            height: 100%;
+            min-width: 20rem;
+            max-width: 90vw;
+            flex: 1 1 auto;
+        }
+    }
 </style>
