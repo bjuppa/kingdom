e743cd40772c00223106513acdae24dbae09ca6e
Björn Nilsved <nilsved@gmail.com>
2018-04-06 16:42:45 +0200

    Illustrate colors and improve wording

--- nuxt/pages/level-1-reset.vue
+++ nuxt/pages/level-1-reset.vue
@@ -5,12 +5,12 @@
             CSS Reset
         </h1>
         <p>
-            The CSS reset is heavily inspired by other great projects like
+            The CSS reset in Kingdom is heavily inspired by other great projects like
             <a href="http://necolas.github.io/normalize.css/">Normalize</a>,
             <a href="http://tinyreset.surge.sh">Tinyreset</a>, and some parts from
             <a href="https://github.com/tailwindcss/tailwindcss/blob/master/css/preflight.css">Tailwind</a>.
-            This documentation aims to cover the bits that are somewhat unique or opinionated.
-            If you seek to understand all the resets included, please look through the source in
+            This documentation aims to cover the resets used in Kingdom that are somewhat unique or opinionated.
+            If you seek to understand all the resets included, please look through the source of
             <code>src/_L1-reset.scss</code>, where comments and links explain most of the individual parts.
         </p>
         <p>
@@ -22,38 +22,35 @@
         <p>
             The reset utilises some basic colors.
             If not set beforehand in the <var>$colors</var> sass-map, the reset will generate these colors
-            automatically and register them with the colors module.
+            automatically and register them with the Kingdom colors module.
             Most of the colors have a matching text color to use on a colored background.
         </p>
-        <table>
-            <caption>Colors</caption>
-            <tr>
-                <th>Primary color</th>
-                <td><code>primary</code></td>
-                <td><code>primary-text</code></td>
-            </tr>
-            <tr>
-                <th>Body background</th>
-                <td><code>body</code></td>
-                <td><code>body-text</code></td>
-            </tr>
-            <tr>
-                <th>Link color</th>
-                <td><code>link</code></td>
-            </tr>
-            <tr>
-                <th>Border color</th>
-                <td><code>border</code></td>
-            </tr>
-        </table>
 
-        <h2>CSS <code>box-sizing</code></h2>
+        <ul>
+            <li>
+                <span class="background-color-primary">
+                    The <code>primary</code> color is used as background together with its corresponding text color.
+                </span>
+            </li>
+            <li>
+                The <code>body</code> color along with its text color can be seen everywhere on this page,
+                but of course also here on this unstyled element.
+            </li>
+            <li>The <a href="#"><code>link</code> color</a> on the body background</li>
+            <li>
+                <div class="border inline-block">The <code>border</code> color defaults to <code>currentColor</code>
+                </div>
+            </li>
+        </ul>
+
+        <h2>Boxes become their set size</h2>
         <p>
-            The reset defaults the <code>box-sizing</code> property of all elements to <code>border-box</code>.
-            Read more about it in <a href="https://css-tricks.com/box-sizing/">the article on CSS-Tricks</a>.
+            To make any explicitly sized elements take up the specified space regardless of padding or borders,
+            the <code>box-sizing</code> property of all elements is set to <code>border-box</code>.
+            Read more about it in <a href="https://css-tricks.com/box-sizing/">this article on CSS-Tricks</a>.
         </p>
 
-        <h2>Borders</h2>
+        <h2>Add borders by setting a single property</h2>
         <p>
             From <a href="https://tailwindcss.com/docs/border-width/">Tailwind</a> comes the nifty trick of setting
             explicit <code>border-style</code> and <code>border-color</code> on all block elements (plus images), while
