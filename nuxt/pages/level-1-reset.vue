d2a13790ec2449fb7caf1f005e5df62d733f2b2a
Björn Nilsved <nilsved@gmail.com>
2018-04-06 12:26:54 +0200

    Add paragraph about inspiring CSS reset projects

--- nuxt/pages/level-1-reset.vue
+++ nuxt/pages/level-1-reset.vue
@@ -1,6 +1,14 @@
 <template>
     <main class="space-inside">
-        <h1>Level 1: CSS Reset</h1>
+        <h1>CSS Reset
+            <small>Level 1</small>
+        </h1>
+        <p>
+            The CSS reset is heavily inspired by other great projects like
+            <a href="http://necolas.github.io/normalize.css/">Normalize</a>,
+            <a href="http://tinyreset.surge.sh">Tinyreset</a>, and some parts from
+            <a href="https://github.com/tailwindcss/tailwindcss/blob/master/css/preflight.css">Tailwind</a>.
+        </p>
     </main>
 </template>
 
