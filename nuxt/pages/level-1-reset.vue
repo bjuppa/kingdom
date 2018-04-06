579dae383f1e591f81020da9eacb2fb7414f41ab
Björn Nilsved <nilsved@gmail.com>
2018-04-06 15:06:12 +0200

    Document colors

--- nuxt/pages/level-1-reset.vue
+++ nuxt/pages/level-1-reset.vue
@@ -9,6 +9,31 @@
             <a href="http://tinyreset.surge.sh">Tinyreset</a>, and some parts from
             <a href="https://github.com/tailwindcss/tailwindcss/blob/master/css/preflight.css">Tailwind</a>.
         </p>
+
+        <h2>Colors</h2>
+        <p>
+            The reset utilises some basic colors.
+            If not set beforehand in the <code>$colors</code> sass-map, the reset will generate these colors
+            automatically and register them with the colors module.
+            Most of the colors have a matching text color to use on a colored background.
+        </p>
+        <table>
+            <caption>Colors</caption>
+            <tr>
+                <th>Primary color</th>
+                <td><code>primary</code></td>
+                <td><code>primary-text</code></td>
+            </tr>
+            <tr>
+                <th>Body background</th>
+                <td><code>body</code></td>
+                <td><code>body-text</code></td>
+            </tr>
+            <tr>
+                <th>Link color</th>
+                <td><code>link</code></td>
+            </tr>
+        </table>
     </main>
 </template>
 
