f25be0ec0f4496ff741c1e5a8d33314a22906504
Björn Nilsved <nilsved@gmail.com>
2018-04-10 08:50:09 +0200

    Display nav in default layout

---
+++ nuxt/components/docs-nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <reload-link to="/">Main</reload-link>
+    </nav>
+</template>
+
+<script>
+  import ReloadLink from '~/components/reload-link.vue';
+
+  export default {
+    components: {
+      'reload-link': ReloadLink
+    }
+  };
+</script>
---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <docs-nav></docs-nav>
+        <nuxt/>
+    </div>
+</template>
+
+<script>
+  import DocsNav from '~/components/docs-nav.vue';
+
+  export default {
+    components: {
+      'docs-nav': DocsNav
+    }
+  };
+</script>
