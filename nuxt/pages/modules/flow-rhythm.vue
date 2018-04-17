81035eb03bf5d98c750e373a4a592b074e421655
Björn Nilsved <nilsved@gmail.com>
2018-04-17 08:54:06 +0200

    Add pages for modules

---
+++ nuxt/pages/modules/flow-rhythm.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="space-inside">
+    <h1>
+      <small>Module:</small>
+      Flow Rhythm
+    </h1>
+    <p>
+      Coming soon
+    </p>
+  </main>
+</template>
+
+<script>
+  import ReloadLink from '~/components/reload-link.vue';
+
+  export default {
+    components: {
+      'reload-link': ReloadLink
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import "~assets/_variables.scss";
+  @import "~assets/L3.scss";
+</style>
