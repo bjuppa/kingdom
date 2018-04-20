69a74784afeb5c81a43fc5da0ab184c6de690ea1
Björn Nilsved <nilsved@gmail.com>
2018-04-20 13:15:37 +0200

    Document sizing module and utils

--- nuxt/pages/modules/sizing.vue
+++ nuxt/pages/modules/sizing.vue
@@ -1,11 +1,34 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Module:</small>
       Sizing
     </h1>
     <p>
-      Coming soon
+      The sizing module contains mixins for working with length related CSS properties.
+      You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/modules/_sizing.scss"
+                           target="_blank"><code>src/modules/sizing.scss</code></a>.
+    </p>
+
+    <h2>Setting general sizes</h2>
+    <p>
+      The <code>set-length</code> mixin is for setting length properties in relation to <var>$spacing-base</var>.
+      When used it will declare the CSS properties but also use CSS calc and the <var>--spacing-base</var> custom
+      property whenever available to make spacing base changeable by scripts.
+    </p>
+
+    <h2>Text sizing</h2>
+    <p>
+      The <code>set-text</code> mixin applies <code>font-size</code> and <code>line-height</code>.
+      If the desired line-height can be related to the <var>$spacing-base</var> it will also be applied with CSS calc,
+      as well as the <var>--line-height</var> custom property that can be used for other nifty calculations
+      (e.g.&nbsp;baseline shift in <reload-link to="/level-4-baseline">Level 4</reload-link>).
+    </p>
+
+    <h2>Measure (line lengths)</h2>
+    <p>
+      The <code>set-readable-max-width</code> mixin applies <code>max-width</code> from <var>$readable-line-length</var>
+      (default&nbsp;<code>75ch</code>).
     </p>
   </main>
 </template>
--- nuxt/pages/utilities/sizing.vue
+++ nuxt/pages/utilities/sizing.vue
@@ -1,12 +1,49 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Utilities:</small>
       Sizing
     </h1>
     <p>
-      Coming soon
+      A collection of classes for setting length related properties.
+      Which classes that are actually generated can be controlled by setting Sass variables.
+      Check the source of <a href="https://github.com/bjuppa/kingdom/blob/master/src/utilities/_sizing.scss"
+                             target="_blank"><code>src/utilities/_sizing.scss</code></a> to see what can be configured.
     </p>
+
+    <h2>Heights (vertical sizing)</h2>
+    <p>
+      Use classes <code>h-<var>n</var></code> for heights that are multiples (<var>n</var>) of the spacing base.
+    </p>
+    <aside class="h-3 border space-inside">
+      For example, this element has class <code>h-3</code> applied and should take up three "lines" of vertical space.
+    </aside>
+    <p>
+      For setting a one-pixel height there's class <code>h-px</code>, that will also set
+      <code>margin-bottom:&nbsp;-1px;</code> to avoid breaking the vertical rhythm.
+    </p>
+
+    <h2>Widths (horizontal sizing)</h2>
+    <p>
+      Horizontally there's classes <code>w-<var>n</var></code> for widths that are multiples (<var>n</var>) of the
+      spacing base.
+      But as we don't need to care about the vertical rhythm, some fractional width classes like <code>w-1/2</code>
+      are available too.
+    </p>
+    <aside class="w-8 border space-inside">
+      For example, this element has class <code>w-8</code> applied and should take upp eight "lines" of horizontal
+      space.
+    </aside>
+
+    <h3>Limiting line lengths</h3>
+    <p>
+      For good readability it's recommended to keep lines of text between 45 and 85 characters long on screens.
+      Class <code>w-readable</code> will apply such a <code>max-width</code> to an element, while classes
+      <code>w-readable-children</code> and <code>w-readable-paragraphs</code> will apply to elements within.
+      This documentation uses the latter to allow headings, lists, and other blocks to be wider, while keeping the
+      body text easy to follow.
+    </p>
+
   </main>
 </template>
 
