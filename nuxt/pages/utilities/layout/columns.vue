20cb448ffd3ed6bc29413d5c190cf1a3058b55d4
Björn Nilsved <nilsved@gmail.com>
2018-06-04 14:01:39 +0200

    Use name-lightness pairs for gray colors

--- nuxt/pages/utilities/layout/columns.vue
+++ nuxt/pages/utilities/layout/columns.vue
@@ -41,7 +41,7 @@
       </p>
 
       <h3>And now, 10 paragraphs of Lorem ipsum&hellip;</h3>
-      <p v-for="item in 10" class="italic color-gray-30">
+      <p v-for="item in 10" class="italic color-gray-300">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
