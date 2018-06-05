de5dc4a48435d2bc5ae80d0e04a5f2ea2a06e2ce
Björn Nilsved <nilsved@gmail.com>
2018-06-05 10:30:36 +0200

    Make breakpoints configurable and set them in css custom properties

--- nuxt/pages/modules/breakpoints.vue
+++ nuxt/pages/modules/breakpoints.vue
@@ -4,8 +4,8 @@
       <small>Module:</small>
       Breakpoints
     </h1>
-    <p>
-      Coming soon
+    <p class="current-device-breakpoint">
+      Current device breakpoint:
     </p>
   </main>
 </template>
@@ -23,4 +23,27 @@
 <style lang="scss">
   @import "~assets/_variables.scss";
   @import "~assets/L3.scss";
+  @import "../../../src/modules/breakpoints";
+
+  /* Demo classes using breakpoint mixins */
+
+  .current-device-breakpoint:after {
+    content: 'n/a';
+
+    @include for-phone-only {
+      content: 'phone';
+    }
+    @include for-tablet-portrait-up {
+      content: 'tablet-portrait';
+    }
+    @include for-tablet-landscape-up {
+      content: 'tablet-landscape';
+    }
+    @include for-desktop-up {
+      content: 'desktop';
+    }
+    @include for-big-desktop-up {
+      content: 'big-desktop';
+    }
+  }
 </style>
