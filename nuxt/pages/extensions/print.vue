746412f7cebb1ed51ccf426bad7b966c446964f0
Björn Nilsved <nilsved@gmail.com>
2018-06-05 14:45:07 +0200

    Add class to hide printing of href

--- nuxt/pages/extensions/print.vue
+++ nuxt/pages/extensions/print.vue
@@ -1,10 +1,10 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Extension:</small>
       Print
     </h1>
-    <p>
+    <p class="print-links-inside">
       The printing module contains utility classes for printing web pages.
       You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/extensions/_print.scss"
                            target="_blank"><code>src/extensions/_print.scss</code></a>.
@@ -14,9 +14,9 @@
     </p>
 
     <h2>Displaying content only for print</h2>
-    <p class="print-only-block italic background-color-gray-100 space-inside float-right w-max-half">
-      This floated paragraph is only displayed in print using class <code>print-only-block</code>.
-    </p>
+    <div class="print-only-block italic border space-inside float-right w-max-half">
+      This floated block is only displayed in print using class <code>print-only-block</code>.
+    </div>
     <p class="mt-0">
       The classes <code>print-only-block</code>, <code>print-only-inline-block</code>, and
       <code>print-only-inline</code> can be used to hide content except when printing.
@@ -29,6 +29,14 @@
         This span is only displayed in print using class
         <code>print-only-inline-block</code>.
       </span>
+    </p>
+
+    <h2 id="printing-links">Printing links</h2>
+    <p>
+      Any <code>article</code> element, or elements with class <code>print-links-inside</code> will have it's contained
+      links printed inline.
+      The class <code>print-href</code> can be used on an <a href="#printing-links" class="print-href">individual
+      element</a> to print its <code>href</code> attribute.
     </p>
   </main>
 </template>
