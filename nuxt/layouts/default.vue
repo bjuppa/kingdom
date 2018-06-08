0d9de27ddd963bb16630828bc434520a1a658ed7
Björn Nilsved <nilsved@gmail.com>
2018-06-08 23:38:07 +0200

    Remove footer wrapper around bottom nav

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -4,13 +4,11 @@
       aria-hidden="true">&#8617;</sub></reload-link>
     <docs-nav></docs-nav>
     <nuxt id="main" class="flex-grow"/>
-    <footer class="background-color-gray-700 mt-2 p-1 print-hidden">
-      <nav class="text-center">
-        <reload-link to="#main" relative class="inline-block p-1 color-inherit">
-          <span aria-hidden="true">&#10224; </span>Back to top of main content<span aria-hidden="true"> &#10224;</span>
-        </reload-link>
-      </nav>
-    </footer>
+    <nav class="background-color-gray-700 mt-2 p-1 text-center">
+      <reload-link to="#main" relative class="inline-block p-1 color-inherit">
+        <span aria-hidden="true">&#10224; </span>Back to top of main content<span aria-hidden="true"> &#10224;</span>
+      </reload-link>
+    </nav>
   </div>
 </template>
 
