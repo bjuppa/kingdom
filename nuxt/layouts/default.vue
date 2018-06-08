61fe62a02e915c1139052d79424bab151e86b1f6
Björn Nilsved <nilsved@gmail.com>
2018-06-08 17:45:50 +0200

    Fix to-top-of-page-link on subpages

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -5,7 +5,7 @@
     <nuxt/>
     <div class="background-color-primary mt-3 space-inside">
       <nav class="text-center">
-        <a href="#top" class="inline-block p-1 color-inherit">&#10224; To top of page &#10224;</a>
+        <reload-link to="#top" relative class="inline-block p-1 color-inherit">&#10224; To top of page &#10224;</reload-link>
       </nav>
     </div>
   </div>
@@ -13,9 +13,11 @@
 
 <script>
   import DocsNav from '~/components/docs-nav.vue';
+  import ReloadLink from '~/components/reload-link';
 
   export default {
     components: {
+      ReloadLink,
       'docs-nav': DocsNav
     }
   };
