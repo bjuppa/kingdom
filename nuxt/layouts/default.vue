3a4d8d232742bffe1f8d39e65244720a31e18d0b
Björn Nilsved <nilsved@gmail.com>
2018-06-08 15:34:45 +0200

    Add back to top link

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <docs-nav></docs-nav>
-        <nuxt/>
+  <div>
+    <docs-nav></docs-nav>
+    <a name="top"></a>
+    <nuxt/>
+    <div class="background-color-primary h-1 mt-3 space-inside">
+      <nav class="text-center">
+        <a href="#top" class="color-inherit no-underline">&uarr;Back to top of page&uarr;</a>
+      </nav>
     </div>
+  </div>
 </template>
 
 <script>
