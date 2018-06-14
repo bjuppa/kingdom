51e998f163b27a040d62b34a25a15179dae3b4c0
Björn Nilsved <nilsved@gmail.com>
2018-06-14 16:49:15 +0200

    Document converters module

--- nuxt/pages/modules/converters.vue
+++ nuxt/pages/modules/converters.vue
@@ -1,12 +1,30 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Module:</small>
       Converters
     </h1>
     <p>
-      Coming soon
+      This module is a collection of functions for converting values between different CSS units.
+      You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/modules/_converters.scss"
+                           target="_blank"><code>src/modules/_converters.scss</code></a>.
     </p>
+    <ul class="w-readable-children">
+      <li>
+        Function <code>length-rem</code> converts any length unit to an approximate equivalent in <code>rem</code>.
+      </li>
+      <li>
+        Function <code>font-size-rem</code> converts any font-size to an approximate equivalent in <code>rem</code>,
+        even from named font-sizes like <code>x-small</code> and <code>larger</code>,
+        as well as relative values in percentage and decimals.
+      </li>
+      <li>
+        Function <code>font-size-px</code> converts any font-size to an approximate equivalent in <code>px</code>.
+      </li>
+      <li>
+        Function <code>font-size-em</code> converts any font-size to an approximate equivalent in <code>em</code>.
+      </li>
+    </ul>
   </main>
 </template>
 
