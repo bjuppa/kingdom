08e8318ad9edf1b114acb81d4e854fe535b06104
Björn Nilsved <nilsved@gmail.com>
2018-06-15 09:12:01 +0200

    Document data-generators

--- nuxt/pages/modules/data-generators.vue
+++ nuxt/pages/modules/data-generators.vue
@@ -1,12 +1,21 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Module:</small>
       Data Generators
     </h1>
     <p>
-      Coming soon
+      This module provides mixins for generating data, mostly used in other modules and to generate placeholders.
+      You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/modules/_data-generators.scss"
+                           target="_blank"><code>src/modules/_data-generators.scss</code></a>.
     </p>
+
+    <h2>Making sure a map contains some whole numbers</h2>
+    <p>
+      Function <code>map-merge-natural-numbers</code> will return a Sass map with all natural (whole) numbers up to
+      <var>$max</var> merged in.
+    </p>
+
   </main>
 </template>
 
