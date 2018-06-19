ce88fbddf6d213ca2b3db7f69cc1cf332dbf0bf3
Björn Nilsved <nilsved@gmail.com>
2018-06-19 08:57:11 +0200

    Documents lists module

--- nuxt/pages/modules/lists.vue
+++ nuxt/pages/modules/lists.vue
@@ -1,12 +1,41 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Module:</small>
       Lists
     </h1>
     <p>
-      Coming soon
+      A collection of mixins for display of <code>&lt;ul&gt;</code> and <code>&lt;ol&gt;</code>.
+      You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/modules/_lists.scss"
+                           target="_blank"><code>src/modules/_lists.scss</code></a>.
     </p>
+
+    <ul class="w-readable-children mt-1-adjacent-children">
+      <li>
+        Mixin <code>list-unstyled</code> removes:
+        <ul class="list-unstyled mt-0">
+          <li>horizontal padding</li>
+          <li>list markers</li>
+        </ul>
+      </li>
+      <li>
+        Mixin <code>list-inline</code> stacks list items horizontally as inline blocks,
+        without affecting children's padding:
+        <ul class="list-inline mt-0">
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+        </ul>
+      </li>
+      <li>
+        Mixin <code>list-comma-separated</code> makes a list inline with separators between elements:
+        <ul class="list-comma-separated">
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+        </ul>
+      </li>
+    </ul>
   </main>
 </template>
 
