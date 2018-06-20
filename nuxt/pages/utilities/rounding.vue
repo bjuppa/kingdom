c6d186dbf37a6a8f08caeabf0d12d2d86a145a13
Björn Nilsved <nilsved@gmail.com>
2018-06-20 15:26:50 +0200

    Improve examples display

--- nuxt/pages/utilities/rounding.vue
+++ nuxt/pages/utilities/rounding.vue
@@ -8,26 +8,26 @@
       Coming soon
     </p>
 
-    <aside class="flex flex-basis-min-content mt-1-children space-children-horizontally w-readable text-center">
-      <div class="rounded-sm background-color-gray-300 space-inside">
+    <aside class="flex mt-1-children space-children-horizontally w-readable text-center space-inside-children">
+      <div class="rounded-sm background-color-gray-100 py-1">
         Small rounded corners with class <code>rounded-sm</code>
       </div>
-      <div class="rounded background-color-gray-300 space-inside">
+      <div class="rounded background-color-gray-100 py-1">
         Medium rounded corners with class <code>rounded</code>
       </div>
-      <div class="rounded-lg background-color-gray-300 space-inside">
+      <div class="rounded-lg background-color-gray-100 py-1">
         Large rounded corners with class <code>rounded-lg</code>
-      </div>
-      <div class="rounded-1 background-color-gray-300 p-1 visualize-vertical-rhythm">
-        Line-high rounded corners with class <code>rounded-1</code>
       </div>
     </aside>
 
-    <aside class="flex mt-1-children space-children-horizontally align-items-center justify-content-space-around w-readable">
-      <code class="rounded-full block h-4 w-4 background-color-gray-300 flex text-center align-items-center">
+    <aside class="flex mt-1-children space-children-horizontally w-readable text-center align-items-center justify-content-space-around ">
+      <div class="rounded-1 background-color-gray-100 py-1 space-inside visualize-vertical-rhythm">
+        Line-high radius with class <code>rounded-1</code>
+      </div>
+      <code class="rounded-full block h-4 w-4 flex-no-shrink background-color-gray-100 flex align-items-center">
         rounded-full
       </code>
-      <code class="rounded-full block pt-1/2 px-1 background-color-gray-300">
+      <code class="rounded-full block flex-no-shrink pt-1/2 px-1/2 background-color-gray-100">
         rounded-full
       </code>
     </aside>
