4e65be4a7476af2778dabd07916b721ead7cf190
Björn Nilsved <nilsved@gmail.com>
2018-06-20 07:28:00 +0200

    Document strip-unit module

--- nuxt/pages/modules/strip-unit.vue
+++ nuxt/pages/modules/strip-unit.vue
@@ -1,11 +1,14 @@
 <template>
-  <main class="space-inside">
+  <main class="space-inside w-readable-paragraphs">
     <h1>
       <small>Module:</small>
       Strip Unit
     </h1>
     <p>
-      Coming soon
+      This module contains a single Sass mixin called <code>strip-unit</code> that does just that.
+      It strips the unit off a Sass variable.
+      You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/modules/_strip-unit.scss"
+                           target="_blank"><code>src/modules/_strip-unit.scss</code></a>.
     </p>
   </main>
 </template>
