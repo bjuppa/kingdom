67a36409055755448bf356bf424ff93eb1881583
Björn Nilsved <nilsved@gmail.com>
2018-06-21 09:25:31 +0200

    Use print: and print-only: prefix on util classes

--- nuxt/pages/extensions/print.vue
+++ nuxt/pages/extensions/print.vue
@@ -4,7 +4,7 @@
       <small>Extension:</small>
       Print
     </h1>
-    <p class="print-links-inside">
+    <p class="print:links-inside">
       The print extension contains utility classes and default styles for printing web pages.
       You'll find it in <a href="https://github.com/bjuppa/kingdom/blob/master/src/extensions/_print.scss"
                            target="_blank"><code>src/extensions/_print.scss</code></a>.
@@ -12,60 +12,60 @@
     <p>
       When printing the <code>&lt;body&gt;</code> is set to black text on white background by default,
       while <code>&lt;nav&gt;</code> and <code>&lt;aside&gt;</code> are hidden completely.
-      <span class="print-hidden">The class <code>print-hidden</code> can be used to remove an element</span>
-      &hellip;<span class="print-invisible"> the class <code>print-invisible</code></span> will hide contents when
+      <span class="print:hidden">The class <code>print:hidden</code> can be used to remove an element</span>
+      &hellip;<span class="print:invisible"> the class <code>print:invisible</code></span> will hide contents when
       printing.
-      Hidden elements can be brought back using classes <code>print-block</code>, <code>print-inline-block</code> &
-      <code>print-inline</code>.
+      Hidden elements can be brought back using classes <code>print:block</code>, <code>print:inline-block</code> &
+      <code>print:inline</code>.
       All <abbr title="abbreviation"><code>&lt;abbr&gt;</code></abbr> elements have their <code>title</code> printed
       after them.
-      <strong class="print-hidden">Print this page as a pdf to see the effects!</strong>
+      <strong class="print:hidden">Print this page as a pdf to see the effects!</strong>
     </p>
 
     <h2>Displaying content only for print</h2>
-    <div class="print-only-block italic border space-inside float-right w-max-half">
-      This floated block is only displayed in print using class <code>print-only-block</code>.
+    <div class="print-only:block italic border space-inside float-right w-max-half">
+      This floated block is only displayed in print using class <code>print-only:block</code>.
     </div>
-    <p class="mt-0 print-background-exact">
-      The classes <code>print-only-block</code>, <code>print-only-inline-block</code>, and
-      <code>print-only-inline</code> can be used to hide content except when printing.
+    <p class="mt-0 print:background-exact">
+      The classes <code>print-only:block</code>, <code>print-only:inline-block</code>, and
+      <code>print-only:inline</code> can be used to hide content except when printing.
 
-      <span class="print-only-inline italic background-color-gray-100">
+      <span class="print-only:inline italic background-color-gray-100">
         This span is only displayed in print using class
-        <code>print-only-inline</code>.
+        <code>print-only:inline</code>.
       </span>
-      <span class="print-only-inline-block italic background-color-gray-100 space-inside">
+      <span class="print-only:inline-block italic background-color-gray-100 space-inside">
         This span is only displayed in print using class
-        <code>print-only-inline-block</code>.
+        <code>print-only:inline-block</code>.
       </span>
     </p>
 
     <h2 id="printing-links">Printing links</h2>
     <p>
-      Any <code>article</code> element, or elements with class <code>print-links-inside</code> will have it's contained
+      Any <code>article</code> element, or elements with class <code>print:links-inside</code> will have it's contained
       links printed inline, if the <code>href</code> contain the protocol.
-      The class <code>print-href</code> can be used on an
-      <reload-link to="#printing-links" relative class="print-href">individual element</reload-link>
+      The class <code>print:href</code> can be used on an
+      <reload-link to="#printing-links" relative class="print:href">individual element</reload-link>
       to print its <code>href</code> attribute.
     </p>
-    <p class="print-links-inside">
-      The class <code>hide-href</code> can be used to override the printing of href on
+    <p class="print:links-inside">
+      The class <code>print:href-none</code> can be used to override the printing of <code>href</code> on
       <a href="https://github.com/bjuppa/kingdom/blob/master/src/extensions/_print.scss"
-         target="_blank" class="hide-href">individual links</a>
+         target="_blank" class="print:href-none">individual links</a>
       which can be useful on image links.
     </p>
 
     <h2>Printing backgrounds</h2>
-    <p class="background-color-gray-100 print-background-exact">
+    <p class="background-color-gray-100 print:background-exact">
       Most browsers won't print backgrounds by default but in WebKit and Firefox the class
-      <code>print-background-exact</code> will make backgrounds print on color printers.
+      <code>print:background-exact</code> will make backgrounds print on color printers.
       If you don't want to print backgrounds, there's a class
-      <code class="background-color-gray-300 print-background-economy">print-background-economy</code>
+      <code class="background-color-gray-300 print:background-economy">print:background-economy</code>
       to override and also
-      <code class="background-color-gray-300 print-background-transparent">print-background-transparent</code>
+      <code class="background-color-gray-300 print:background-transparent">print:background-transparent</code>
       that will remove the background and set color to <code>inherit</code> for contrast on black & white printers
       too.
-      Class <code>print-remove-shadows</code> will remove any box- and text-shadows when printing.
+      Class <code>print:shadow-none</code> will remove any box- and text-shadows when printing.
     </p>
   </main>
 </template>
