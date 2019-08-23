f64c91d67224344cc940db92c459b0cf49e45028
Björn Nilsved <nilsved@gmail.com>
2019-08-23 13:05:46 +0200

    Correct mixin names for docs

--- nuxt/pages/modules/sizing.vue
+++ nuxt/pages/modules/sizing.vue
@@ -12,14 +12,14 @@
 
     <h2>Setting general sizes</h2>
     <p>
-      The <code>set-length</code> mixin is for setting length properties in relation to <var>$spacing-base</var>.
+      The <code>length-relative</code> mixin is for setting length properties in relation to <var>$spacing-base</var>.
       When used it will declare the CSS properties but also use CSS calc and the <var>--spacing-base</var> custom
       property whenever available to make the spacing base changeable client-side.
     </p>
 
     <h2>Text sizing</h2>
     <p>
-      The <code>set-text</code> mixin applies <code>font-size</code> and <code>line-height</code>.
+      The <code>size-text</code> mixin applies <code>font-size</code> and <code>line-height</code>.
       If the desired line-height can be related to the <var>$spacing-base</var> it will also be applied with CSS calc,
       as well as the <var>--line-height</var> custom property that can be used for other nifty calculations
       (e.g.&nbsp;baseline shift in <reload-link to="/level-4-baseline">Level&nbsp;4</reload-link>).
@@ -27,7 +27,7 @@
 
     <h2>Measure (line lengths)</h2>
     <p>
-      The <code>set-readable-max-width</code> mixin applies <code>max-width</code> from <var>$readable-line-length</var>
+      The <code>width-readable</code> mixin applies <code>max-width</code> from <var>$readable-line-length</var>
       (default&nbsp;<code>75ch</code>).
     </p>
 
